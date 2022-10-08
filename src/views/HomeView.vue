b0d960637b9608bf2006e0c70c892a8b257c9dad
Anton Kolot <antvelkol@gmail.com>
2022-10-08 20:33:01 +0300

    Easily add and display a to-do list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,44 @@
 <template>
   <div>
+    <h2>Create your to-do list</h2>
+    <div>
+      <input v-model="title" placeholder="Title"/>
+      <input v-model="description" placeholder="Description"/>
+      <button @click="createNewToDo">Create</button>
+    </div>
+
+    <div>
+      <h3>Current to-do list</h3>
+      <div v-for="todo in todoList" :key="todo.id">
+        <p>Title: {{todo.title}}</p>
+        <p>Description: {{todo.description}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
+  name: 'Create your to-do list',
+  data() {
+    return {
+      title: '',
+      description: '',
+      todoList: []
+    }
+  },
+  methods: {
+    createNewToDo() {
+      let test = {id: Date.now(), title: this.title, description: this.description}
+      this.todoList.push(test);
+      localStorage.setItem('todoList', JSON.stringify(this.todoList))
+    }
+  },
+  mounted() {
+    const currentTodoList = localStorage.getItem('todoList');
+    if(currentTodoList && Array.isArray(currentTodoList)){
+      this.todoList.push(localStorage.getItem('todoList'));
+    }
+  }
 }
 </script>
