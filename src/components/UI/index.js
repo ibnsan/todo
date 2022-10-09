bf9a167612545086ec848571a65f5ccd170bd4f1
Anton Kolot <antvelkol@gmail.com>
2022-10-09 10:21:22 +0300

    UI components connected to the whole project

---
+++ src/components/UI/index.js
@@ -0,0 +1,9 @@
+import TodoButton from "@/components/UI/buttons/TodoButton";
+import TodoInput from "@/components/UI/inputs/TodoInput";
+import TodoTextarea from "@/components/UI/TodoTextarea";
+
+export default [
+    TodoButton,
+    TodoInput,
+    TodoTextarea
+];
