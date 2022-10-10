1cc1f138c177f961a8ba939d9e88e7f302b25aba
Anton Kolot <antvelkol@gmail.com>
2022-10-10 12:04:16 +0300

    Decomposition to create a new project

---
+++ src/components/Project/CreateProject.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="relative mb-4">
+    <todo-input v-model="projectName" placeholder="Project name"/>
+  </div>
+  <todo-button @click="createNewProject">Create</todo-button>
+</template>
+
+<script>
+export default {
+  name: 'create-project',
+  props: {
+    projectList: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      projectName: '',
+      nameSelectedProject: '',
+    }
+  },
+  methods: {
+    createNewProject() {
+      if (this.projectName.length >= 3) {
+        const newProject = {id: Date.now(), name: this.projectName, value: this.projectName, status: 'backLog'};
+        this.$emit('savedProjects', newProject);
+        localStorage.setItem('projectList', JSON.stringify(this.projectList));
+      }
+    },
+  }
+}
+</script>
