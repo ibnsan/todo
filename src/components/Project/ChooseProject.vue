92e3878bf3b4e7eca7ac340bdee636fdfbc4b5f3
Anton Kolot <antvelkol@gmail.com>
2022-10-10 12:02:10 +0300

    Removed irrelevant header and updated emit

--- src/components/Project/ChooseProject.vue
+++ src/components/Project/ChooseProject.vue
@@ -1,5 +1,4 @@
 <template>
-  <h2 class=" mx-auto text-gray-900 text-lg font-medium title-font mb-5">Choose the project</h2>
   <div class="relative mb-4">
     <todo-select v-model="projectName" @update:model-value="setSelectedValue" :options="projectList" />
   </div>
@@ -24,7 +23,7 @@ export default {
   },
   methods: {
     setSelectedValue() {
-      this.$emit('select', this.projectName);
+      this.$emit('selectedProject', this.projectName);
     }
   }
 }
