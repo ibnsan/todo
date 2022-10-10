13412760ce10001b1f0977e939c6928e547dc336
Anton Kolot <antvelkol@gmail.com>
2022-10-10 12:57:56 +0300

    Decomposition of the show todo list

---
+++ src/components/Todo/ShowTodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mx-auto lg:w-2/6 w-full">
+    <div v-for="todo in todoList" :key="todo.id" class="bg-gray-100 rounded flex p-4 h-full  flex flex-col md:mt-5">
+      <span class="title-font">Title: {{ todo.title }}</span>
+      <span class="title-font">Description: {{ todo.description }}</span>
+      <span class="title-font">Project: {{ todo.project }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todoList: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
